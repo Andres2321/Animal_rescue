f11f867a59a0ea948ace0d9987d6bac9e4f143c6
Andres <andres-a23@live.com>
2020-04-10 14:39:42 -0400

    deploying heroku

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-  baseURL: Process.env.NODE_ENV === 'production' ? 'https://animal-rescue-001.herokuapp.com/' : 'http://localhost:3000'
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://animal-rescue-001.herokuapp.com/' : 'http://localhost:3000'
 });
 
 // =====================Auth========================
